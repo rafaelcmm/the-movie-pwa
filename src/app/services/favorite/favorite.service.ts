c98b2df47266977fb2bc297944054438ac7eb28a
rafaelcm95 <rafael@labelnet.com.br>
2018-06-19 11:41:56 -0300

    integração de serviço

--- src/app/services/favorite/favorite.service.ts
+++ src/app/services/favorite/favorite.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class FavoriteService {
 
-  constructor() { }
+  public favorites: Observable<any[]>;
 
+  constructor(
+    private db: AngularFireDatabase
+  ) {
+    this.favorites = this.db.list('/starred').valueChanges();
+  }
 }
