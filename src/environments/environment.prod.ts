51174ad48626ce8549a1444609f4990b02b1dae2
rafaelcm95 <rafael@labelnet.com.br>
2018-06-19 11:37:14 -0300

    alterações nos pacotes e credenciais do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
   apiKey: 'd17c3475585769f8e34fb35ed646cd58',
-  baseUrl: 'https://api.themoviedb.org/3/'
+  baseUrl: 'https://api.themoviedb.org/3/',
+  firebase: {
+    apiKey: "AIzaSyBn8vygKIm6tVtccVoOUXcqWsXB84JKVmM",
+    authDomain: "teste-movie.firebaseapp.com",
+    databaseURL: "https://teste-movie.firebaseio.com",
+    projectId: "teste-movie",
+    storageBucket: "teste-movie.appspot.com",
+    messagingSenderId: "926321019400"
+  }
 };
